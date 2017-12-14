973d3e0c64503283cfe13614245507786caa9fa2
GramBam <graham.moss@hotmail.com>
2017-12-13 21:59:39 -0500

    Add test todos

--- src/App.js
+++ src/App.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var todos = [
+  {
+    todoTitle: 'My First Todo',
+    todoResponsible: 'Graham',
+    todoDescription: 'My first todo description',
+    todoPriority: 'low'
+  },
+  {
+    todoTitle: 'My Second Todo',
+    todoResponsible: 'Graham',
+    todoDescription: 'My second todo description',
+    todoPriority: 'medium'
+  },
+  {
+    todoTitle: 'My Third Todo',
+    todoResponsible: 'Graham',
+    todoDescription: 'My third todo description',
+    todoPriority: 'high'
+  }
+]
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h4>Todo Count: <span className="badge">{this.state.todos.length}</span></h4>
+        <ul className="list-group">
+          {this.state.todos.map((todo, index) =>
+            <li className="list-group-item" key={index}>
+              <h4 className="list-group-item-heading">{todo.todoTitle}</h4>
+            </li>
+          )}
+        </ul>
       </div>
     );
   }
